4dcf4b81e49a33d008e5189a443a68283d1d82f6
Michael <mfw6mw@virginia.edu>
2022-01-09 13:55:03 -0500

    Reseeded DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,45 +7,153 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Spidey.create(
-    image: "https://i.pinimg.com/564x/9a/5e/2b/9a5e2bfe9a6ed176cf28fbf037f20332.jpg",
+    image: "https://icon-library.com/images/icon-spiderman/icon-spiderman-24.jpg",
     alias: "Spiderman",
     surname: "Peter Parker",
     year: 1992
 )
 
 Spidey.create(
-    image: "https://i.pinimg.com/564x/9a/5e/2b/9a5e2bfe9a6ed176cf28fbf037f20332.jpg",
+    image: "https://pm1.narvii.com/7022/799c5b30642fae316672b02d9f3c21efc1bd8923r1-1080-991v2_128.jpg",
     alias: "Spider Pig",
     surname: "Peter Porker",
     year: 1993
 )
 
 Spidey.create(
-    image: "https://i.pinimg.com/564x/9a/5e/2b/9a5e2bfe9a6ed176cf28fbf037f20332.jpg",
+    image: "https://64.media.tumblr.com/98e9514ac49a5d48e24491fe1cd3b511/tumblr_outh1sVEqt1w7t6tto2_r1_400.png",
     alias: "Spider Gwen",
     surname: "Gwen Stacy",
     year: 1987
 )
 
 Spidey.create(
-    image: "https://i.pinimg.com/564x/9a/5e/2b/9a5e2bfe9a6ed176cf28fbf037f20332.jpg",
-    alias: "Mecha Spiderman",
+    image: "https://64.media.tumblr.com/d0a09e3867de9a9546f06c5920bb0e22/90d32a58da403de5-53/s500x750/169cd6231df5cc1780eec0136860c39daac06aba.png",
+    alias: "Spiderman Noir",
     surname: "Larry Cables",
     year: 1978
 )
 
 Spidey.create(
-    image: "https://i.pinimg.com/564x/9a/5e/2b/9a5e2bfe9a6ed176cf28fbf037f20332.jpg",
+    image: "https://auntm.ai/resources/ui/uigacha/featured/gachachaseprize_256x256_venom.png",
     alias: "Dark Spiderman",
     surname: "Eddie Brock",
     year: 1992
 )
 
 Spidey.create(
-    image: "https://i.pinimg.com/564x/9a/5e/2b/9a5e2bfe9a6ed176cf28fbf037f20332.jpg",
+    image: "https://64.media.tumblr.com/a5aea51b3aa1d797ba1203c5c9039a19/tumblr_outh1sVEqt1w7t6tto1_400.png",
     alias: "Spiderman",
     surname: "Miles Morales",
     year: 2018
+)
+
+Comic.create(
+    name: "The Amazing Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Spectacular Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Incredible Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Best Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Neighborhood Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Spectacular Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Best Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Fantabulous Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Greatest Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Amazing Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Spectacular Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Incredible Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Best Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Neighborhood Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Spectacular Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Best Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Fantabulous Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
+)
+
+Comic.create(
+    name: "The Greatest Spiderman",
+    year: rand(1960..2022),
+    spidey_id: rand(1..6),
 )
 
 Comic.create(
